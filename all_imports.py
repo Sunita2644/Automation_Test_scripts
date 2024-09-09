1238ddcb0270e0e6ddb572105d37aa789731d127
Sunita2644 <sunita.tanawade@yahoo.com>
2024-09-09 15:01:39 +0530

    script added for register user form

--- all_imports.py
+++ all_imports.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 from selenium.webdriver.common.by import By
 import time
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
