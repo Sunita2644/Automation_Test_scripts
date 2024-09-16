bdc972f523194aef1f64dafe86f0a04efc8345cf
Sunita2644 <sunita.tanawade@yahoo.com>
2024-09-16 18:41:56 +0530

    code added for user login with valid creds

---
+++ User_Login_CorrectCred.py
@@ -0,0 +1,35 @@
+from all_imports import *
+
+class User_login(unittest.TestCase):
+
+    def setUp(self):
+        driver.get("https://automationexercise.com/")
+        ele=driver.find_element(By.XPATH,"//div[@class='logo pull-left']")
+        if ele.is_displayed():
+            print("Url opened successfully")
+        time.sleep(1)
+
+    def test_loginUser(self):
+        driver.find_element(By.XPATH,"//a[@href='/login']").click()
+        time.sleep(1)
+        ele2=driver.find_element(By.XPATH,"//*[@id='form']/div/div/div[1]/div/h2")
+        if ele2.is_displayed():
+            print("login form opened")
+        driver.find_element(By.XPATH,"//input[@data-qa='login-email']").send_keys("tests@testss.com")
+        driver.find_element(By.XPATH,"//input[@data-qa='login-password']").send_keys("test")
+        driver.find_element(By.XPATH,"//button[@data-qa='login-button']").click()
+        ele3 = driver.find_element(By.XPATH, "//*[@id='header']/div/div/div/div[2]/div/ul/li[10]")
+        try:
+            if ele3.text == 'Logged in as test':
+                print("logged in successfully")
+        except Exception as e:
+            print(e)
+        driver.find_element(By.XPATH, "//a[@href='/delete_account']").click()
+        ele4 = driver.find_element(By.XPATH, "//h2[@data-qa='account-deleted']")
+        if ele4.text == 'ACCOUNT DELETED!':
+            print("Account deleted successfully")
+
+    def tearDown(self):
+        driver.close()
+
+
